5a2182c478f243780d35568e0ffc61fab95044c1
DuskWarden <pawar96sameer@gmail.com>
2025-11-29 14:44:06 +0530

    add section title component

--- frontend/src/components/UI/SectionTitle.js
+++ frontend/src/components/UI/SectionTitle.js
@@ -12,7 +12,7 @@ const styles = StyleSheet.create({
         fontSize: 18,
         fontWeight: 'bold',
         color: COLORS.text,
-        marginBottom: 10,
+        marginBottom: 12,
     },
 });
 
